97571c08651ff8e758e453cb0252cee2960abadf
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-09-22 01:47:57 +0200

    Added first hello text

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,15 @@ import { NavigationBar } from "@/components/navbar/Navbar";
 export default function App() {
   return (
     <main>
-      {/* Background */}
-      <div className="fixed inset-0 w-screen h-screen">
-        {/* <Silk
-          speed={5}
-          scale={1}
-          color="#7B7481"
-          noiseIntensity={1.5}
-          rotation={0}
-        /> */}
-      </div>
-
       {/* Navigation */}
       <div className="fixed bottom-0 left-1/2 -translate-x-1/2 z-20">
         <NavigationBar />
       </div>
 
       {/* Greetings */}
-      <section></section>
+      <section>
+        <h1 className="text-4xl font-bold">Hello, I'm Alex</h1>
+      </section>
 
       {/* Skills */}
       <section></section>
