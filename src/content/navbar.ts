60da2bdf4da75d242e5e8235b5591c66547f515a
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-09-24 01:50:02 +0200

    Testing CI

--- src/content/navbar.ts
+++ src/content/navbar.ts
@@ -45,4 +45,6 @@ export const NAVBAR_ITEMS: Navbar = [
     type: "button",
     item: { label: "Github", icon: Github, onClick: () => openURL(GithubURL) },
   },
+  { type: "separator" },
+  { type: "theme-toggle" }
 ];
