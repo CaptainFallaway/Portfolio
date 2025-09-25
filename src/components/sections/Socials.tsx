3a045436afe34a2d7c401de281da34ea54323af1
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-09-25 10:28:56 +0200

    refactor: update About component to display Socials heading

--- src/components/sections/Socials.tsx
+++ src/components/sections/Socials.tsx
@@ -1,7 +1,3 @@
 export default function About() {
-  return (
-    <h2 className="text-2xl font-semibold">
-      Experiences, Competence, What I like to work on.
-    </h2>
-  );
+  return <h2 className="text-2xl font-semibold">Socials.</h2>;
 }
