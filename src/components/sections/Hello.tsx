779ace560d097997fb9a02eb4872d1044be07ae4
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-09-25 10:41:31 +0200

    fix: adjust Hello section height for better responsiveness

--- src/components/sections/Hello.tsx
+++ src/components/sections/Hello.tsx
@@ -34,7 +34,7 @@ function Welcome({ welcome }: { welcome: WelcomeType }) {
 
 export default function Hello() {
   return (
-    <div className="flex items-center gap-4 justify-center h-[72vh] w-[82vw] md:w-[68vw]">
+    <div className="flex items-center gap-4 justify-center h-fit w-[82vw] md:w-[68vw]">
       <div className="flex gap-4 md:flex-row flex-col">
         <div className="card p-6 w-full h-fit flex justify-center">
           <div className="flex h-fit w-full items-start">
